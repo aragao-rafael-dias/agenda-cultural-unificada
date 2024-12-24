cff7a9233ae420eebf775fe5e1579faa7019d218
Rafael <rafaeldiasaragao@gmail.com>
2024-12-23 23:07:41 -0300

    modified:   .gitignore 	modified:   app.py 	modified:   static/js/script.js 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -25,3 +25,5 @@ def get_cult():
 # Finalizando o APP
 if __name__ == '__main__':
     app.run(debug=True)
+
+# RAFFS DIAS
--- static/js/script.js
+++ static/js/script.js
@@ -8,10 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 eventoDiv.innerHTML = `
                 <p><strong>Nome:</strong> ${evento.nome}</p>
                 <p><strong>Data:</strong> ${evento.data}</p>
-                <p><strong>Organização:</strong> ${evento.org}</p>
+                <p><strong>Organização:</strong> ${evento.org}</p><hr>
                 `;
                 eventos.appendChild(eventoDiv);
             });
         })
 })
 .catch(error => console.error('Erro ao carregar eventos', error));
+
+// RAFFS DIAS
